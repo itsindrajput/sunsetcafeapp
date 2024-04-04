41452be8596e214f1feb2c3dd192923920200b59
Rishabh Singh <itsindrajput@gmail.com>
2024-04-04 11:40:39 +0530

    Fixed Some UI

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "../../components/Menu/Navbar";
+
 const Menu = () => {
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
       <div>Menu</div>
     </>
   );
